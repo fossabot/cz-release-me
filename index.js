5add1d421ba2d4efc52129053a297c8e357a98dc
Rafał Wolak <rafal@tomandco.co.uk>
2017-06-09 13:45:24 +0200

    feat(package): removed babel converter, reorganized code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const editor = require('editor');
 const temp = require('temp').track();
 const fs = require('fs');
 const path = require('path');
+const shell = require('shelljs');
 
 let log = require('winston');
 let buildCommit = require('./lib/build-commit');
@@ -13,6 +14,8 @@ let changelogrcConfig = require('./lib/changelogrc-config');
 module.exports = {
   prompter: (cz, commit) => {
     let config = changelogrcConfig();
+    let pkgPath = path.resolve(process.cwd(), './package.json')
+    let pkg = require(pkgPath);
 
     Promise.resolve(config).then((value) => {
       log.info('\n\nLine 1 will be cropped at 100 characters. All other lines will be wrapped after 100 characters.\n');
@@ -47,6 +50,26 @@ module.exports = {
         }
         else {
           log.info('Commit has been canceled.');
+        }
+
+        /* istanbul ignore if */
+        if (answers.releaseMe === 'yes') {
+          if (pkg.scripts.release) {
+            shell.exec('npm run release');
+          } else {
+            log.info('Release script is not defined.');
+          }
+        }
+
+        /* istanbul ignore if */
+        if (answers.pushChanges=== 'yes') {
+          shell.exec('git push --follow-tags');
+
+          if (answers.releaseMe === 'yes' && pkg.scripts.release) {
+            shell.exec('npm publish');
+          } else {
+            log.info('No new release, nothing to push to the npm.');
+          }
         }
       });
     });
